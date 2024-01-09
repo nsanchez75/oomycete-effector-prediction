9a6eafa8a6d3acde1f544cc7c2041a88da287ca0
Nikko Sanchez <n.sanchez7580@gmail.com>
2024-01-08 17:26:57 -0800

    delete commented code

--- app.py
+++ app.py
@@ -299,35 +299,10 @@ def tab_content(active_tab):
 																	'''),])
 
 
-# create app HTML skeleton
+# create app
 app.layout = html_content.create_app_skeleton(header, fasta_input_card, info_card, table_card)
-# app.layout = html.Div(
-# 	[
-# 		header,
-# 		dcc.Markdown('''If you use EffectorO in your research, please cite us:'''),
-# 		html.Blockquote('Nur, M. J., Wood, K. J. & Michelmore, R. W. EffectorO: motif-independent prediction of effectors in oomycete genomes using machine learning and lineage-specificity. Mol. Plant-Microbe Interact. (2023). doi:10.1094/MPMI-11-22-0236-TA'),
-# 		dcc.Markdown('''
-# 									Also — if you are using EffectorO to predict oomycete effectors
-# 									we would love to hear from you! Please email Kelsey at
-# 									[klsywd@gmail.com](mailto:klsywd@gmail.com) or on Twitter
-# 									[@klsywd](https://twitter.com/klsywd) and let us know what
-# 									organism you are studying.
-# 									'''),
-# 		dcc.Interval(
-# 			id='interval-component',
-# 			interval=2000,  # Refresh the memory usage every 2 seconds
-# 			n_intervals=0
-# 		),
-# 		dbc.Container(
-# 			[dbc.Row([dbc.Col(image_card, md=6),
-# 								dbc.Col(card, md=6)]),
-# 			dbc.Row([dbc.Col(table_card)])],
-# 			fluid=True,
-# 		),
-# 		html.Div(id="memory-usage"),
-# 	]
-# )
 
+# initialize callback functions
 
 # we use a callback to toggle the collapse on small screens
 @app.callback(
